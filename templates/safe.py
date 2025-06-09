b370df4ef1c1de00d4a9395e58347ee220090af6
喜乐 <zpu@pku.edu.cn>
2025-06-09 20:03:17 +0800

    Create safe.py

---
+++ templates/safe.py
@@ -0,0 +1,12 @@
+from random import randrange
+
+
+"""
+codeforces-python: 算法竞赛Python3模板库
+#1: Python int 随机抑或, 对抗哈希攻击
+https://github.com/xile42/codeforces-python/blob/main/templates/safe.py
+"""
+RANDOM_KEY = randrange(1 << 29)  # 2 ^ 29: 避免生成的随机数超出普通整数范围, 避免大整数运算, 提高性能; 不同版本(pypy/python3, 32bit/64bit)这个范围不同, 2 ^ 29是通用保险范围
+
+def XorRandomly(x):
+  return x ^ RANDOM_KEY
