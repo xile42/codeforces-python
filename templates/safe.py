18be71af3f0b722834f2823c53ce1fa9a5368252
xile42 <zpu@pku.edu.cn>
2025-06-10 00:16:25 +0800

    Add templates.

--- templates/safe.py
+++ templates/safe.py
@@ -8,5 +8,5 @@ https://github.com/xile42/codeforces-python/blob/main/templates/safe.py
 """
 RANDOM_KEY = randrange(1 << 29)  # 2 ^ 29: 避免生成的随机数超出普通整数范围, 避免大整数运算, 提高性能; 不同版本(pypy/python3, 32bit/64bit)这个范围不同, 2 ^ 29是通用保险范围
 
-def XorRandomly(x):
+def XorRandomly(x: int) -> int:
     return x ^ RANDOM_KEY
