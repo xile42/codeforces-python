9cd4a8d17f5fb819e6ce9234d50f37fa8acfff97
喜乐 <zpu@pku.edu.cn>
2025-07-26 13:35:17 +0800

    Update prefix_sum.py

--- templates/prefix_sum.py
+++ templates/prefix_sum.py
@@ -101,18 +101,19 @@ https://github.com/xile42/codeforces-python/blob/main/templates/prefix_sum.py
 """
 class DistancePrefixSum:
 
-    def __init__(self, arr: List[int]):
+    def __init__(self, arr: List[int], is_sorted: bool = False):
 
         self.n = len(arr)
-        self.arr = sorted(arr)
+        self.arr = sorted(arr) if not is_sorted else arr
         self.prefix = [0] * (self.n + 1)
 
         for i in range(self.n):
             self.prefix[i + 1] = self.prefix[i] + self.arr[i]
 
-    def query(self, target: int, l: int=0, r: Optional[int]=None) -> int:
+    def query(self, target: Optional[int]=None, l: int=0, r: Optional[int]=None) -> int:
         """ sum(abs(a[i] - target)) for i in [l, r] """
 
+        target = self.arr[self.n // 2] if target is None else target
         r = self.n - 1 if r is None else r
         i = bisect.bisect_left(self.arr, target, l, r + 1)
         left = target * (i - l) - (self.prefix[i] - self.prefix[l])
